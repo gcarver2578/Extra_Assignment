513c9bff8a0b25e8e69ecb2c996f6f5cef418ec8
Gabriel Carver <37904423+gcarver2578@users.noreply.github.com>
2019-01-19 17:01:07 -0600

    Update dice_prob.py
    done

--- dice_prob.py
+++ dice_prob.py
@@ -1,6 +1,30 @@
 # Gabriel Carver
 import random
 import graphics as g
+# import time
+# important stuff that needs to be accessed in different functions
+_close_ = g.Rectangle(g.Point(275, 0), g.Point(325, 50))
+close_text = g.Text(g.Point(300, 25), 'Done')
+_close_.setFill('black')
+_roll_ = g.Rectangle(g.Point(275, 50), g.Point(325, 100))
+roll_text = g.Text(g.Point(300, 75), 'Roll')
+_roll_.setFill(g.color_rgb(98, 166, 255))
+_sim_ = g.Rectangle(g.Point(275, 100), g.Point(325, 150))
+sim_text = g.Text(g.Point(300, 125), 'Sim')
+_sim_.setFill('blue')
+_res_ = g.Rectangle(g.Point(275, 150), g.Point(325, 200))
+res_text = g.Text(g.Point(300, 175), 'Reset')
+_res_.setFill('red')
+inp = g.Entry(g.Point(-20, 0), 10)
+num_rolls = -1
+perfect_rolls = -1
+n_text = g.Text(g.Point(100, 420), 'Rolls: 0')
+p_text = g.Text(g.Point(100, 435), 'Five of a Kinds: 0')
+per_text = g.Text(g.Point(250, 435), '0%')
+draw_stuff = [_close_, close_text, _sim_, sim_text, _roll_, roll_text, inp, n_text, p_text, per_text, _res_, res_text]
+for _item_ in [close_text, roll_text, sim_text, res_text]:
+    _item_.setStyle('bold')
+    _item_.setFill('white')
 
 
 def main():
@@ -8,33 +32,58 @@ def main():
 
 
 def dice():
+    global num_rolls, perfect_rolls
     d_win = g.GraphWin("Dice", 325, 450)
     d_win.setBackground('White')
-    _close_ = g.Rectangle(g.Point(275, 0), g.Point(325, 50))
-    close_text = g.Text(g.Point(300, 25), 'Done')
-    _close_.setFill('red')
-    close_text.setStyle('bold')
-    close_text.setFill('white')
-    draw_stuff = [_close_, close_text]
-    for item in draw_stuff:
-        item.draw(d_win)
-    rolls = [0, 0, 0, 0, 0]
-    for i in range(5):
-        rolls[i] = random.randint(1, 6)
+    rolls = [1] * 5
     draw_dice(rolls, d_win)
-    d_win.getMouse()
-    clear(d_win)
-    d_win.getMouse()
+    update_stats(rolls)
+    m = d_win.getMouse()
+    while True:
+        while not inside([m], _close_.getP1(), _res_.getP2()):
+            m = d_win.getMouse()
+        # ends the program
+        if inside([m], _close_.getP1(), _close_.getP2()):
+            d_win.close()
+            return
+        # does one roll
+        elif inside([m], _roll_.getP1(), _roll_.getP2()):
+            clear(d_win)
+            rolls = reroll(rolls)
+            draw_dice(rolls, d_win)
+            update_stats(rolls)
+            m = d_win.getMouse()
+        # simulates rolls until a 5 of a kind
+        elif inside([m], _sim_.getP1(), _sim_.getP2()):
+            inp.setText('')
+            rolls = reroll(rolls)
+            while rolls.count(rolls[0]) != 5 and ' ' not in inp.getText():
+                clear(d_win)
+                rolls = reroll(rolls)
+                draw_dice(rolls, d_win)
+                update_stats(rolls)
+            m = d_win.getMouse()
+        else:
+            num_rolls = -1
+            perfect_rolls = -1
+            rolls = [1] * 5
+            clear(d_win)
+            update_stats(rolls)
+            draw_dice(rolls, d_win)
+            m = d_win.getMouse()
 
 
 def draw_dice(rolls, d_win):
-    print(rolls)
+    for item in draw_stuff:
+        item.draw(d_win)
     starting_points_y = [25, 125, 225]
     colors = ["Red", "Green", "Blue", "Black", "Orange", "Gold", "Cyan", "Pink", "Purple"]
     r_index = 0
-    # prints out dice without numbers
+    # prints out dice without dots
     for s_x in starting_points_y[:2]:
         for s_y in starting_points_y:
+            if r_index == 5:
+                break
             points = [g.Point(0 + s_x, 0 + s_y), g.Point(0 + s_x, 75 + s_y), g.Point(75 + s_x, 75 + s_y),
                       g.Point(75 + s_x, 0 + s_y)]
             square = g.Polygon(points)
@@ -42,34 +91,37 @@ def draw_dice(rolls, d_win):
             square.setFill(colors[random.randint(0, len(colors) - 1)])
             # cases for which dots to draw
             if rolls[r_index] == 1:
-                continue
+                dots = [g.Point(s_x + 37.5, s_y + 37.5)]
             elif rolls[r_index] == 2:
-                continue
+                dots = [g.Point(s_x + 12.5, s_y + 12.5), g.Point(s_x + 62.5, s_y + 62.5)]
             elif rolls[r_index] == 3:
-                continue
+                dots = [g.Point(s_x + 12.5, s_y + 62.5), g.Point(s_x + 37.5, s_y + 37.5),
+                        g.Point(s_x + 62.5, s_y + 12.5)]
             elif rolls[r_index] == 4:
-                continue
+                dots = [g.Point(s_x + 12.5, s_y + 12.5), g.Point(s_x + 12.5, s_y + 62.5),
+                        g.Point(s_x + 62.5, s_y + 12.5), g.Point(s_x + 62.5, s_y + 62.5)]
             elif rolls[r_index] == 5:
-                continue
+                dots = [g.Point(s_x + 12.5, s_y + 12.5), g.Point(s_x + 12.5, s_y + 62.5),
+                        g.Point(s_x + 62.5, s_y + 12.5), g.Point(s_x + 62.5, s_y + 62.5),
+                        g.Point(s_x + 37.5, s_y + 37.5)]
+            elif rolls[r_index] == 6:
+                dots = [g.Point(s_x + 12.5, s_y + 12.5), g.Point(s_x + 12.5, s_y + 62.5),
+                        g.Point(s_x + 62.5, s_y + 12.5), g.Point(s_x + 62.5, s_y + 62.5),
+                        g.Point(s_x + 12.5, s_y + 37.5), g.Point(s_x + 62.5, s_y + 37.5)]
             else:
-                continue
-    # points for each side of the dice
-    s_1 = [g.Point(25 + 37.5, 25 + 37.5)]
-    s_2 = [g.Point(25 + 12.5, 125 + 12.5), g.Point(25 + 62.5, 125 + 62.5)]
-    s_3 = [g.Point(25 + 12.5, 225 + 62.5), g.Point(25 + 37.5, 225 + 37.5), g.Point(25 + 62.5, 225 + 12.5)]
-    s_4 = [g.Point(125 + 12.5, 25 + 12.5), g.Point(125 + 12.5, 25 + 62.5), g.Point(125 + 62.5, 25 + 12.5),
-           g.Point(125 + 62.5, 25 + 62.5)]
-    s_5 = [g.Point(125 + 12.5, 125 + 12.5), g.Point(125 + 12.5, 125 + 62.5), g.Point(125 + 62.5, 125 + 12.5),
-           g.Point(125 + 62.5, 125 + 62.5), g.Point(125 + 37.5, 125 + 37.5)]
-    s_6 = [g.Point(125 + 12.5, 225 + 12.5), g.Point(125 + 12.5, 225 + 62.5), g.Point(125 + 62.5, 225 + 12.5),
-           g.Point(125 + 62.5, 225 + 62.5), g.Point(125 + 12.5, 225 + 37.5), g.Point(125 + 62.5, 225 + 37.5)]
-    sides = [s_1, s_2, s_3, s_4, s_5, s_6]
-    # prints out dots on dice
-    for side in sides:
-        for center in side:
-            c = g.Circle(center, 7)
-            c.draw(d_win)
-            c.setFill("White")
+                dots = []
+            for dot in dots:
+                c = g.Circle(dot, 7)
+                c.draw(d_win)
+                c.setFill('white')
+            r_index += 1
+
+
+# rerolls the dice
+def reroll(rolls):
+    for i in range(len(rolls)):
+        rolls[i] = random.randint(1, 6)
+    return rolls
 
 
 # clears the screen
@@ -77,6 +129,24 @@ def clear(win):
     for item in win.items[:]:
         item.undraw()
     win.update()
+
+
+def update_stats(rolls):
+    global num_rolls, perfect_rolls
+    num_rolls += 1
+    str1 = 'Rolls: ' + str(num_rolls)
+    n_text.setText(str1)
+    if rolls.count(rolls[0]) == 5:
+        perfect_rolls += 1
+    str2 = 'Five of a Kinds: ' + str(perfect_rolls)
+    p_text.setText(str2)
+    str3 = ''
+    try:
+        str3 = str(round((perfect_rolls / num_rolls) * 100, 4)) + '%'
+    except ZeroDivisionError:
+        str3 = '0.0%'
+    finally:
+        per_text.setText(str3)
 
 
 # returns if any of the given points is inside a given box
